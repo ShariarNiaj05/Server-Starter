04df721e7ab2df396fae8ecb24905fa00b792a1c
ShariarNiaj05 <shariarn85@gmail.com>
2024-07-09 00:57:58 +0600

    starter server updated in app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,28 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
-import notFound from "./app/middlewares/notFound";
-import router from "./app/routes";
-import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+import notFound from './app/middlewares/notFound'
+import router from './app/routes'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import config from './app/config'
 
-const app: Application = express();
+const app: Application = express()
 
 //parsers
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
 // application routes
-app.use("/api/v1", router);
+app.use('/api/v1', router)
 
 const test = async (req: Request, res: Response) => {
-  // Promise.reject();
-  const a = 10;
-  res.send(a);
-};
+  res.send(`Server is running on port: ${config.port}`)
+}
 
-app.get("/", test);
+app.get('/', test)
 
-app.use(globalErrorHandler);
+app.use(globalErrorHandler)
 
 //Not Found
-app.use(notFound);
+app.use(notFound)
 
-export default app;
+export default app
